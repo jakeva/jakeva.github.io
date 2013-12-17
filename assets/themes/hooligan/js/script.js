b9d12699e31939ceb3ad6a218f797675516b299e
Jake Van Alstyne <jakeva@gmail.com>
2013-12-17 16:17:27 -0700

    going to need that script

---
+++ assets/themes/hooligan/js/script.js
@@ -0,0 +1,12 @@
+function codeAddress() {
+    var _jvObfuscatedHREF0 = "mai";
+    var _jvObfuscatedHREF1 = "lto";
+    var _jvObfuscatedHREF2 = ":jak";
+    var _jvObfuscatedHREF3 = "eva";
+    var _jvObfuscatedHREF4 = "@gm";
+    var _jvObfuscatedHREF5 = "ail";
+    var _jvObfuscatedHREF6 = ".co";
+    var _jvObfuscatedHREF7 = "m";
+    var _jvObfuscatedHREF  = _jvObfuscatedHREF0+_jvObfuscatedHREF1+_jvObfuscatedHREF2+_jvObfuscatedHREF3+_jvObfuscatedHREF4+_jvObfuscatedHREF5+_jvObfuscatedHREF6+_jvObfuscatedHREF7;document.getElementById('email_contact').href = _jvObfuscatedHREF;
+}
+window.onload = codeAddress;
