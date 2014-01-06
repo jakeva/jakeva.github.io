2ab7c6c198e3265504608d3fbb662ad189f27bf9
Jake Van Alstyne <jakeva@gmail.com>
2014-01-05 20:43:10 -0700

    updated email

--- assets/themes/hooligan/js/script.js
+++ assets/themes/hooligan/js/script.js
@@ -2,9 +2,9 @@ function codeAddress() {
     var _jvObfuscatedHREF0 = "mai";
     var _jvObfuscatedHREF1 = "lto";
     var _jvObfuscatedHREF2 = ":jak";
-    var _jvObfuscatedHREF3 = "eva";
-    var _jvObfuscatedHREF4 = "@gm";
-    var _jvObfuscatedHREF5 = "ail";
+    var _jvObfuscatedHREF3 = "e@e";
+    var _jvObfuscatedHREF4 = "ggde";
+    var _jvObfuscatedHREF5 = "vil";
     var _jvObfuscatedHREF6 = ".co";
     var _jvObfuscatedHREF7 = "m";
     var _jvObfuscatedHREF  = _jvObfuscatedHREF0+_jvObfuscatedHREF1+_jvObfuscatedHREF2+_jvObfuscatedHREF3+_jvObfuscatedHREF4+_jvObfuscatedHREF5+_jvObfuscatedHREF6+_jvObfuscatedHREF7;document.getElementById('email_contact').href = _jvObfuscatedHREF;
