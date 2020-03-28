259f934cdad443171f4acd3385e827012e254f3c
Jake Van Alstyns <jakeva@gmail.com>
2020-03-28 18:34:33 +0000

    Travis #13

--- assets/themes/jakeva/js/script.js
+++ assets/themes/jakeva/js/script.js
@@ -1,12 +1,15 @@
 function codeAddress() {
+  if (!!document.getElementById('email_contact')) {
     var _jvObfuscatedHREF0 = "mai";
     var _jvObfuscatedHREF1 = "lto";
     var _jvObfuscatedHREF2 = ":jak";
-    var _jvObfuscatedHREF3 = "e@e";
-    var _jvObfuscatedHREF4 = "ggde";
-    var _jvObfuscatedHREF5 = "vil";
+    var _jvObfuscatedHREF3 = "eva@g";
+    var _jvObfuscatedHREF4 = "mai";
+    var _jvObfuscatedHREF5 = "l";
     var _jvObfuscatedHREF6 = ".co";
     var _jvObfuscatedHREF7 = "m";
-    var _jvObfuscatedHREF  = _jvObfuscatedHREF0+_jvObfuscatedHREF1+_jvObfuscatedHREF2+_jvObfuscatedHREF3+_jvObfuscatedHREF4+_jvObfuscatedHREF5+_jvObfuscatedHREF6+_jvObfuscatedHREF7;document.getElementById('email_contact').href = _jvObfuscatedHREF;
+    var _jvObfuscatedHREF  = _jvObfuscatedHREF0+_jvObfuscatedHREF1+_jvObfuscatedHREF2+_jvObfuscatedHREF3+_jvObfuscatedHREF4+_jvObfuscatedHREF5+_jvObfuscatedHREF6+_jvObfuscatedHREF7;
+    document.getElementById('email_contact').href = _jvObfuscatedHREF;
+  }
 }
 window.onload = codeAddress;
